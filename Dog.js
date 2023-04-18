0056072a53c40d9231441d242b7fd77c9d4784a7
adenning4 <41348927+adenning4@users.noreply.github.com>
2023-04-17 18:07:03 -0600

    js functionality
    full functional working, no visuals

--- Dog.js
+++ Dog.js
@@ -1,1 +1,40 @@
-// Create the Dog class here
+class Dog {
+  constructor(data) {
+    Object.assign(this, data);
+  }
+
+  bannerSource() {
+    if (this.hasBeenLiked) {
+      return "./images/badge-like.png";
+    } else {
+      return "./images/badge-nope.png";
+    }
+  }
+
+  getBannerHtml() {
+    if (this.hasBeenSwiped) {
+      return `
+        <div class="banner"><img src="${this.bannerSource()}" /></div>
+        `;
+    } else {
+      return ``;
+    }
+  }
+
+  getProfileHtml() {
+    const { name, avatar, age, bio, hasBeenSwiped, hasBeenLiked } = this;
+    return `
+            <div id="prospect">
+                ${this.getBannerHtml()}
+                <p class="prospect-info">${name}, ${age}</p>
+                <p class="prospect-bio">${bio}</p>
+            </div>
+            <div class="buttons">
+                <button class="button dislike-btn" data-like-or-dislike = 'dislike' >dislike</button>
+                <button class="button like-btn" data-like-or-dislike = 'like' >like</button>
+            </div>
+        `;
+  }
+}
+
+export default Dog;
--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Remember to import the data and Dog class!
+import dogs from "./data.js";
+import Dog from "./Dog.js";
+
+let isWaiting = false;
+
+function getNewDog() {
+  const nextDogData = dogs.shift();
+  return nextDogData ? new Dog(nextDogData) : null;
+}
+
+function setLikeStatus(dog, likeOrDislike) {
+  if (!isWaiting) {
+    dog.hasBeenSwiped = true;
+    dog.hasBeenLiked = likeOrDislike === "like";
+    isWaiting = true;
+    render();
+    currentDog = getNewDog();
+    if (currentDog) {
+      setTimeout(() => {
+        render();
+        isWaiting = false;
+      }, 1500);
+    }
+  }
+}
+
+function render() {
+  document.querySelector("main").innerHTML = currentDog.getProfileHtml();
+  document.getElementById(
+    "prospect"
+  ).style.backgroundImage = `url(${currentDog.avatar})`;
+}
+
+let currentDog = getNewDog();
+render();
+
+document.addEventListener("click", function (e) {
+  if (e.target.classList.contains("button")) {
+    setLikeStatus(currentDog, e.target.dataset.likeOrDislike);
+  }
+});
